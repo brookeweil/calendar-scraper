16f0fbe52e4ceba3431c76061a43d568b13e8022
Brooke Weil <brooke.weil@buzzfeed.com>
2023-06-25 18:57:33 -0400

    no default for test scrape

--- src/test_scrape.js
+++ src/test_scrape.js
@@ -8,5 +8,6 @@ urls.splice(0, 2); // remove 2 items from index 0; subsequent args are user inpu
 if (urls.length) {
     generateIcsData(urls).then((result) => console.log('done'))
 } else {
-    generateIcsData(['https://www.google.com']).then((result) => console.log(result))
+    // generateIcsData(['https://www.google.com']).then((result) => console.log(result))
+    console.log('No input URLs provided')
 }
